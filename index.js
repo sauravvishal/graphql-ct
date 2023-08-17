b72d22b496376b78a3dd49e43fdd06eb14b64def
Saurav Vishal <saurav.vishal@overdose.digital>
2023-08-17 11:49:34 +0530

    changed index.js

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-import { app } from "./src/server.js";
+import express from "express";
+import { createServer } from "http";
+import { ApolloServer } from "apollo-server-express";
+import dotenv from "dotenv";
 
-app.startServer();
+import { typeDefs } from "./src/graphql/schemas/schema.js";
+import { resolvers } from "./src/graphql/resolvers/resolver.js";
+
+dotenv.config();
+
+class App {
+    async startServer() {
+        try {
+            const app = express();
+            const server = new ApolloServer({ typeDefs, resolvers });
+            await server.start();
+            server.applyMiddleware({ app, path: '/graphql' });
+
+            const httpServer = createServer(app);
+
+            httpServer.listen(process.env.PORT, () => {
+                console.log(`Server is running on port ${process.env.PORT}`);
+            });
+        } catch (error) {
+            console.log("Internal Server Error", error);
+        }
+    }
+}
+
+new App().startServer();
